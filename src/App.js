148ac4b99fbb0f96f30436869d2ba4af32f357a7
Martin Mendoza <mendozanmartin@gmail.com>
2021-01-13 19:07:58 -0500

    integrate with backend

--- src/App.js
+++ src/App.js
@@ -4,72 +4,40 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  let [clothingItems, setClothingItems] = useState([]);
+  let [events, setEvents] = useState([]);
 
   useEffect(() => {
     const getItems = async () => {
       await axios({
         method: "GET",
-        url: "http://localhost:5000/get-items",
+        url: "http://localhost:5000/events",
         headers: {
           "content-type": "application/json",
         },
       })
         .then((response) => {
-          setClothingItems(response.data);
+          setEvents(response.data);
         })
         .catch((error) => {
           console.log(error);
         });
     };
     getItems();
-  }, [clothingItems, setClothingItems]);
-
-  const items = [
-    {
-      title: "Denim Jeans",
-      price: 29.99,
-      image:
-        "https://www.blackdiamondequipment.com/on/demandware.static/-/Sites-bdel/default/dw0f24a23b/products/S19_Product_Images/S19_Apparel/750020_4010_FORGEDDENIM_MFORGEDDENIMPANTS.jpg",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget.",
-    },
-    {
-      title: "Polo shirt",
-      price: 19.99,
-      image:
-        "https://www.prada.com/content/dam/pradanux_products/U/UJN/UJN444/1C61F0ZON/UJN444_1C61_F0ZON_S_181_SLF.png",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget.",
-    },
-    {
-      title: "Polo shirt",
-      price: 19.99,
-      image:
-        "https://www.prada.com/content/dam/pradanux_products/U/UJN/UJN444/1C61F0ZON/UJN444_1C61_F0ZON_S_181_SLF.png",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget.",
-    },
-    {
-      title: "Polo shirt",
-      price: 19.99,
-      image:
-        "https://www.prada.com/content/dam/pradanux_products/U/UJN/UJN444/1C61F0ZON/UJN444_1C61_F0ZON_S_181_SLF.png",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget.",
-    },
-  ];
+  }, [events, setEvents]);
 
   return (
     <div className="App">
       <h1>MFC Admin Portal</h1>
       <div className="container">
         <div className="row">
-          {clothingItems.map((item, i) => (
+          {events.map((item, i) => (
             <div className="col-4" key={i}>
               <div className="item-container">
                 <h3>{item.title}</h3>
-                <p> {item.description}</p>
+                <p> {item.date}</p>
+                <p> {item.time}</p>
+                <p> {item.venue}</p>
+                <p> {item.image}</p>
                 <div className="d-flex justify-content-center">
                   <img src={item.image} style={{ maxWidth: "150px" }}></img>
                 </div>
